7423aaab457f7139896c530f1c6cf9407fcf4c95
osawa-koki <github@osawa.dev>
2023-03-01 19:50:52 +0900

    小さな修正。

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,5 @@
 variable "project_name" {
   type = string
   description = "Name of the project"
-}
-
-variable "allowed_ip_address" {
-  type = string
-  description = "IP address to allow"
+  default = "simple-cosmosdb-mongodb"
 }
