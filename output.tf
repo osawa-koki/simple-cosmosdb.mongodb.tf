c6842169e7d8545efc01a6d599345cb2c953e09d
osawa-koki <osawa-koki@example.com>
2023-03-01 14:02:34 +0900

    出力。

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 
 output "cosmosdb_connection_string" {
-  value = "mongodb://${azurerm_cosmosdb_account.example.name}.mongo.cosmos.azure.com:${azurerm_cosmosdb_account.example.primary_master_key}@${azurerm_cosmosdb_account.example.name}:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@${azurerm_cosmosdb_account.example.name}@"
+  value = "mongodb://${azurerm_cosmosdb_account.example.name}.mongo.cosmos.azure.com:${azurerm_cosmosdb_account.example.primary_key}@${azurerm_cosmosdb_account.example.name}:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@${azurerm_cosmosdb_account.example.name}@"
+  sensitive = true
 }
